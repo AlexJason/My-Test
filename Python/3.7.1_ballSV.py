87a5b7ec731dd2db7d8c71f0a8bace2de8f4c35b
AlexJason <cuizhihui030925@outlook.com>
2018-05-15 23:15:14 +0800

    Initial commit

---
+++ Python/3.7.1_ballSV.py
@@ -0,0 +1,14 @@
+# ballSV.py
+
+import math
+
+def main():
+	r = float(input('Input the R:'))
+	V = (math.pi * (r ** 3) * 4) / 3
+	S = 4 * math.pi * (r ** 2)
+	print('V=', V, '\nS=', S)
+
+	input('Press <Enter> to continue...')
+	return 0
+
+main()
